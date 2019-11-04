f36e041e355bc6be1f007fec2fb7f14b238dff97
Marko Kovačina <m-kovacina@users.noreply.github.com>
2019-11-04 15:58:14 +0100

    TAI-3: Initial commit

---
+++ samples/Geta.oEmbed.Sample/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using Geta.oEmbed.Sample.Models.ViewModels;
+
+namespace Geta.oEmbed.Sample.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
---
+++ samples/Geta.oEmbed.Sample/Models/Blocks/SiteBlockData.cs
@@ -0,0 +1,10 @@
+
+namespace Geta.oEmbed.Sample.Models.Blocks
+{
+    /// <summary>
+    /// Base class for all block types on the site
+    /// </summary>
+    public abstract class SiteBlockData : EPiServer.Core.BlockData
+    {
+    }
+}
